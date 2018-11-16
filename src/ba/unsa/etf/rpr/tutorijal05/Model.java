dc1916b3f8c7310d397c69364e06b2dedde907b2
avujcic1 <avujcic1@etf.unsa.ba>
2018-11-16 17:33:03 +0100

    Novi model i style

--- src/ba/unsa/etf/rpr/tutorijal05/Model.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Model.java
@@ -1,19 +1,22 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
-public class Model {
 
-    public double calculate(long n1, long n2, String operator) {
-        switch(operator) {
+public class Model {
+    public double calculate(long number1, long number2, String operator){
+        switch (operator){
             case "+":
-                return n1+n2;
+                return number1+number2;
             case "-":
-                return n1-n2;
+                return number1-number2;
             case "*":
-                return n1*n2;
+                return number1*number2;
             case "/":
-                if(n2==0) return 0;
-                return n1/n2;
+                if(number2==0) return 0;
+                return number1/number2;
+
+
             default: return 0;
+
         }
     }
 }
